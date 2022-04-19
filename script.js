ff671eb4fc312742124f2418750d9c813909827d
jaymicrocode <jaycode128@gmail.com>
2022-04-19 11:27:20 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+let firstName, lastName, website;
+
+[firstName, lastName, website] = ['Oluwatobi', 'Sofela', 'codesweetly.com'];
+
+console.log(firstName);
+console.log(lastName);
+console.log(website);
